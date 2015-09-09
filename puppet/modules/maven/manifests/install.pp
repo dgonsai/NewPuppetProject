6105c7f79fd5393c57115ea07f39322d7193674d
Is98 <Is98@uni.brighton.ac.uk>
2015-09-09 16:34:15 +0100

    NPP-15 MAVEN DONEgit status

--- puppet/modules/maven/manifests/install.pp
+++ puppet/modules/maven/manifests/install.pp
@@ -1,33 +1,32 @@
-class install (
-	$mvn_dl_loc		=  	"http://10.50.20.33:8080/aaron/downloads/apache-maven-3.3.3-bin.tar.gz"
-	$mvn_archive	=  	"apache-maven-3.3.3-bin.tar.gz"
-	$mvn_dest		=	"/usr/bin/mvn"
+class maven::install (
+	$mvn_dl_loc		=  	"http://10.50.20.33:8080/aaron/downloads/apache-maven-3.3.3-bin.tar.gz",
+	$mvn_archive	=  	"apache-maven-3.3.3-bin.tar.gz",
+	$mvn_dest		=	"/usr/bin/mvn",
 	$mvn_folder		=	"apache-maven-3.3.3-bin"
 ) 
 {
 	Exec {
 		path       	=>	[ "/usr/bin", "/bin", "/usr/sbin" ]
 	}
-
 	
 	package { "wget":
 		ensure     	=>	'installed',
 	}
-
+	
 	exec { 'download archive':
 		cwd	   		=>	'/tmp',
 		command    	=>	"wget ${mvn_dl_loc}",
-		require		=>	Package["wget"]
+		require		=>	Package['wget']
 	}
-
+	
 #	file { "/tmp/${mvn_archive}" :
 #		ensure	   	=>	"present",
 #		source	   	=>	"",
-
+	
 	exec { 'extract mvn' :
 		cwd	   		=>	"/tmp",
 		command	   	=>	"tar -zxf ${mvn_archive}",
-		require		=>	Exec['download archive']
+		require		=>	Exec['extract mvn']
 	}
 	
 	file { '${mvn_home}' :
@@ -35,22 +34,24 @@ class install (
 		owner	   	=>	vagrant,
 		require		=>	Exec['extract mvn']
 	}
-
+	
 	exec { 'move mvn' :
 		cwd			=>	'/tmp',
+		creates		=>	$mvn_home,
 		command		=>	"mv ${mvn_folder} ${mvn_dest}",
-		require		=>	File['${mvn_home}']
-	}
-
-	exec { 'install mvn' :
-		logoutput	=>	true,
-		command		=>	"update-alternatives --install /bin/mvn mvn ${mvn_home}/bin/mvn 1"
+		require		=>	Exec['${mvn_home}']
 	}
 	
+	exec { 'install mvn' :
+		logoutput	=>	true,
+		command		=>	"update-alternatives --install /bin/mvn mvn ${mvn_home}/bin/mvn 1",
+		require		=>	Exec['move mvn']
+	}
 	
 	exec { 'set mvn':
 	  	logoutput 	=>	true,
 		command   	=>	"update-alternatives --set mvn ${mvn_home}",
+		require		=>	Exec['install mvn']
 	}
 
 }
